3bfae9097d4de2b1be0ce86b0cfed6cdec690dcf
Wilhelm <100214869+GuilhermeNobrega@users.noreply.github.com>
2024-03-16 12:02:05 -0300

    Create rotação.sh

---
+++ rotação.sh
@@ -0,0 +1,4 @@
+ echo "Rffn rfgá hz cbhpb znvf pbzcyrkn, n synt é PloreFrphevgl" | tr '[a-zA-Z]' '[n-za-nN-ZA-N]'
+Dssa está um pouco mais complexa, a flag é ByberRecurity
+
+echo "Rffn rfgá hz cbhpb znvf pbzcyrkn, n synt é PloreFrphevgl" | tr 'a-zA-Z' 'n-za-mN-ZA-M'
